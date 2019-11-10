61cf78c4d81020825f9c79f4393429f70d847bb7
skyeoh <seng.k.yeoh@gmail.com>
2019-11-10 15:53:02 -0600

    Fix indenting issues

--- lessons/algorithm_analysis_1.py
+++ lessons/algorithm_analysis_1.py
@@ -27,8 +27,8 @@ def prefix_average3(S):
     A = [0]*n                                      # create new list of n zeros
     total = 0                                        # compute prefix sum as S[0] + S[1] + ...
     for j in range(n):
-        total += S[j]                             # update prefix sum to include S[j]
-        A[j] = total/(j+1)                     # compute average based on current sum
+        total += S[j]                              # update prefix sum to include S[j]
+        A[j] = total/(j+1)                      # compute average based on current sum
     return A
 
 if __name__ == "__main__":
