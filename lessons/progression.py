8a345c0bb5e75c436d068fb67cdf89aa99e1cd8c
skyeoh <seng.k.yeoh@gmail.com>
2019-11-10 19:29:43 -0600

    Add more class, inheritance lesson

---
+++ lessons/progression.py
@@ -0,0 +1,44 @@
+# Goodrich, Tamassia, Goldwasser (2013) Code Fragment 2.8 on page 88
+
+class Progression:
+    """Iterator producing a generic progression.
+
+    Default iterator produces the whole numbers 0, 1, 2, ...
+    """
+
+    def __init__(self, start=0):
+        """Initialize current to the first value of the progression. """
+        self._current = start
+
+    def _advance(self):
+        """Update self._current to a new value.
+
+        This should be overridden by a subclass to customize progression.
+
+        By convention, if current is set to None, this designates the
+        end of a finite progression.
+        """
+        self._current += 1
+
+    def __next__(self):
+        """Return the next element, or else raise StopIteration error."""
+        if self._current is None:            # our convention to end a progression
+            raise StopIteration()
+        else:
+            answer = self._current          # record current value to return
+            self._advance()                     # advance to prepare for next time
+            return answer                        # return the answer
+
+    def __iter__(self):
+        """By convention, an iterator must return itself as an iterator."""
+        return self
+
+    def print_progression(self, n):
+        """Print next n values of the progression."""
+        print('  '.join(str(next(self)) for j in range(n)))
+
+if __name__ == "__main__":
+    seq1 = Progression()
+    seq1.print_progression(10)
+    seq2 = Progression(2)
+    seq2.print_progression(20)
