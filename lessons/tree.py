4a6de26f6cd2d43a3ac6e29dc571e91fc3596729
skyeoh <seng.k.yeoh@gmail.com>
2020-01-09 12:46:55 -0600

    Add implementations of postorder and breadth-first traversal methods for Tree and inorder traversal method for BinaryTree

--- lessons/tree.py
+++ lessons/tree.py
@@ -1,4 +1,5 @@
 # Goodrich, Tamassia, Goldwasser (2013) Code Fragments 8.1-8.6 on pages 307-310
+from linked_queue import LinkedQueue
 
 class Tree:
     """Abstract base class representing a tree structure."""
@@ -101,3 +102,27 @@ class Tree:
         for c in self.children(p):                                 # for each child c
             for other in self._subtree_preorder(c):       # do preorder of c's subtree
                 yield other                                              # yielding each to our caller
+
+    def postorder(self):
+        """Generate a postorder iteration of positions in the tree."""
+        if not self.is_empty():
+            for p in self._subtree_postorder(self.root()):          # start recursion
+                yield p
+
+    def _subtree_postorder(self, p):
+        """Generate a postorder iteration of positions in subtree rooted at p."""
+        for c in self.children(p):                                 # for each child c
+            for other in self._subtree_postorder(c):     # do postorder of c's subtree
+                yield other                                              # yielding each to our caller
+        yield p                                                            # visit p after its subtrees
+
+    def breadthfirst(self):
+        """Generate a breadth-first iteration of the positions of the tree."""
+        if not self.is_empty():
+            fringe = LinkedQueue()                      # known positions not yet yielded
+            fringe.enqueue(self.root())                  # starting with the root
+            while not fringe.is_empty():
+                p = fringe.dequeue()                       # remove from front of the queue
+                yield p                                             # report this position
+                for c in self.children(p):
+                    fringe.enqueue(c)                        # add children to back of queue
