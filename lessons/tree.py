71cef96812848413f482ffe37e0bd87a2ff8bc0a
skyeoh <seng.k.yeoh@gmail.com>
2020-01-08 20:46:38 -0600

    Add implementations of iter, positions and preorder tree traversal

--- lessons/tree.py
+++ lessons/tree.py
@@ -79,3 +79,25 @@ class Tree:
         if p is None:
             p = self.root()
         return self._height2(p)                       # start _height2 recursion
+
+    def __iter__(self):
+        """Generate an iteration of the tree's elements."""
+        for p in self.positions():            # use same order as positions()
+            yield p.element()                   # but yield each element
+
+    def positions(self):
+        """Generate an iteration of the tree's positions."""
+        return self.preorder()                    # return entire preorder iteration
+
+    def preorder(self):
+        """Generate a preorder iteration of positions in the tree."""
+        if not self.is_empty():
+            for p in self._subtree_preorder(self.root()):           # start recursion
+                yield p
+
+    def _subtree_preorder(self, p):
+        """Generate a preorder iteration of positions in subtree rooted at p."""
+        yield p                                                            # visit p before its subtrees
+        for c in self.children(p):                                 # for each child c
+            for other in self._subtree_preorder(c):       # do preorder of c's subtree
+                yield other                                              # yielding each to our caller
