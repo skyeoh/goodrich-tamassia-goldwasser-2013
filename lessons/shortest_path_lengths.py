2ec09b8f0be7546376e7d43071751987f0d9d991
skyeoh <seng.k.yeoh@gmail.com>
2020-02-05 14:06:27 -0600

    Add implementation of finding shortest-path tree

--- lessons/shortest_path_lengths.py
+++ lessons/shortest_path_lengths.py
@@ -1,4 +1,4 @@
-# Goodrich, Tamassia, Goldwasser (2013) Code Fragment 14.13 on page 668
+# Goodrich, Tamassia, Goldwasser (2013) Code Fragment 14.13-14.14 on page 668-669
 from adaptable_heap_priority_queue import AdaptableHeapPriorityQueue
 
 def shortest_path_lengths(g, src):
@@ -37,5 +37,21 @@ def shortest_path_lengths(g, src):
                     pq.update(pqlocator[v], d[v], v)               # update the pq entry
 
     return cloud                                             # only includes reachable vertices
+
+def shortest_path_tree(g, s, d):
+    """Reconstruct shortest-path tree rooted at vertex s, given distance map d.
+
+    Return tree as a map from each reachable vertex v (other than s) to the
+    edge e=(u, v) that is used to reach v from its parent u in the tree.
+    """
+    tree = {}
+    for v in d:
+        if v is not s:
+            for e in g.incident_edges(v, False):         # consider INCOMING edges
+                u = e.opposite(v)
+                wgt = e.element()
+                if d[v] == d[u] + wgt:
+                    tree[v] = e                                        # edge e is used to reach v
+    return tree
 
 if __name__ == "__main__":
